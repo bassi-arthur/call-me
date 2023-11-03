0a4be69cfc64907c4c2c0369cbbacc73d8264d35
Arthur Bassi <arthur.winiarski.ranger@outlook.com>
2023-11-03 11:46:28 -0400

    fat: Add export default to component

--- src/pages/schedule/[username]/ScheduleForm/index.page.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.page.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { CalendarStep } from "./CalendarStep/index.page";
+import CalendarStep from "./CalendarStep/index.page";
 import ConfirmStep from "./ConfirmStep";
 
 export default function ScheduleForm() {
